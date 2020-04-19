f509de4f6b50a772fb64b3ee790d191bd834a868
Yasmin Hansen <yaa-h@hotmail.com>
2020-04-19 19:22:00 -0300

    Lista todos os compromissos cadastrados

--- backend/src/controller/ScheduleController.js
+++ backend/src/controller/ScheduleController.js
@@ -1,6 +1,22 @@
 const connection = require('../database/connection');
 
 module.exports = {
+    async index(request, response){
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('schedule').count();
+        // armazena o numero de compromissos cadastrados pelo usuario
+
+        const schedule = await connection('schedule')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select('*');
+
+        response.header('X-Total-Count', count['count(*)']);
+
+        return response.json(schedule);
+    },
+
     async create(request, response){
         const { date, description, commitment } = request.body;
         const user_id = request.headers.authorization;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -17,19 +17,21 @@ routes.get('/user', UserController.index);
 // O método acima lista todos os usuários
 // Esse método será excluído futuramente
 routes.post('/user', UserController.create); //cria a rota de criação do usuário
-
-routes.get('/profile', ProfileController.index); //exibe todos os pets cadastrados
+routes.get('/profile', ProfileController.index); //exibe todos os pets cadastrados pelo usuário no seu perfil
 routes.get('/profile/schedule', ProfileController.scheduleIndex); //exibe todos os compromissos cadastrados pelo usuário
 
 //Rotas relacionada ao cadastro, edição e deleção de pet
-routes.get('/pets', PetController.index); //lista todos os pets
+routes.get('/pets', PetController.index); //lista TODOS os pets
 routes.post('/pets', PetController.create); //cria a rota de criação de pet
 routes.delete('/pets/:id', PetController.delete) //deleta um pet cadastrado
 
 // Rotas relacionadas ao cadastro de novos compromissos que serão usados na agenda
 routes.post('/commitment', CommitmentController.create); //cria um novo compromisso
 
-// Rota da agenda, onde são cadastrados os compromissos juntos ccom a descrição e horário
+// Rota da agenda, onde são cadastrados os compromissos juntos com a descrição e horário
+routes.get('/schedule', ScheduleController.index); //lista TODOS os compromissos
 routes.post('/schedule', ScheduleController.create); // cria um novo compromisso na agenda
 routes.delete('/schedule/:id', ScheduleController.delete); // deleta um compromisso que foi criado na agenda
+
+
 module.exports = routes; //exporta as rotas
